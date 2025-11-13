88b06ef1d9b8e8459e15b26d8215d7314231f75d
somenath singh <144797336+itsnarutouzumaki@users.noreply.github.com>
2025-11-13 13:36:06 +0530

    proxy link fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'https://food-delivery-2yg5.onrender.com'
     }
   }
 })
