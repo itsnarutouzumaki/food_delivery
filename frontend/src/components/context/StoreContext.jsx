7513c59bd10452f659373ada706c2242ec5482c7
somenath singh <144797336+itsnarutouzumaki@users.noreply.github.com>
2025-11-13 13:53:11 +0530

    URL correction

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-2yg5.onrender.com";
   const [token, setToken] = useState("");
 
   //remove food_list state 
